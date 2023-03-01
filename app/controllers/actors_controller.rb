7acc960c71c6012ba63ce9bca111f9310b57cde9
jdowns525 <downsjosseph@gmail.com>
2023-03-01 18:24:13 +0000

    added /actors and /movies

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,23 @@
+class ActorsController < ApplicationController 
+
+  def index
+
+    @list_of_actors = Actor.all
+    
+    render({ :template => "actor_templates/index.html.erb" })
+  end
+
+  
+  def actor_details
+    # params looks like {"an_id"=>"42"}
+
+    the_id = params.fetch("an_id")
+
+    @the_actor = Actor.where({ :id => the_id }).at(0)
+
+    @filmography = Character.where({ :actor_id => @the_actor.id }).at(0)
+    
+    render({ :template => "actor_templates/show.html.erb" })
+  end
+
+end
