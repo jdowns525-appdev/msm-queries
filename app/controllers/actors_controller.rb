ed8af5eafb68b76e158f56376ecfb6fdd97eef54
jdowns525 <downsjosseph@gmail.com>
2023-03-02 15:22:24 +0000

    commit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,7 +14,7 @@ class ActorsController < ApplicationController
     the_id = params.fetch("an_id")
 
     @the_actor = Actor.where({ :id => the_id }).at(0)
-
+    
     @character = Character.where({ :actor_id => @the_actor.id })
 
     render({ :template => "actor_templates/show.html.erb" })
